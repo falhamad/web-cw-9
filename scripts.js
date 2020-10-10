c08b337b4b945065c91853c824afb03016453458
fatma al hamad <fatmaalhamadd@gmail.com>
2020-10-10 20:03:01 +0300

    cw 9

--- scripts.js
+++ scripts.js
@@ -1,12 +1,35 @@
-// 1
-// moon.jpg قم بالوصول إلى الصورة وتغيرها إلى
-
-// 2
-// "قم بالوصول إلى كلمة الأرض وتحويلها إلى "القمر
+// // 1
+// // moon.jpg قم بالوصول إلى الصورة وتغيرها إلى
+// let image = document.getElementById('image');
+// image.src = "./images/moon.jpg";
+// // 2
+// // "قم بالوصول إلى كلمة الأرض وتحويلها إلى "القمر
+// let word = document.getElementById('caption');
+// console.log('word')
+// word.innerHTML = "القمر"
 
 // 3
 // بحيث إن ضغطت على البطاقة تتغير الصورة للصورة الأخرى ويتغير الاسم onClick قم بإنشاء دالة واستخدام خاصية
 // دون الحاجة إلى الخطوتين 2و1
+function sunMoon () {
+let word = document.getElementById('caption')
+
+if (word.innerHTML=="القمر"){
+
+word.innerHTML = 'الشمس';
+let image = document.getElementById('image')
+image.src='./images/sun.jpg';
+document.body.style.background = "white";
+}
+
+else {
+
+word.innerHTML = 'القمر';
+let image = document.getElementById('image')
+image.src='./images/moon.jpg';
+document.body.style.background = "grey";
+}
+}
 
 // 4
 // Night - Light Mode قم بالتعديل على الدالة بحيث تضم إليها خاصية
